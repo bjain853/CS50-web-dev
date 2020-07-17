0e4cce6e76ffa45d2819a8053d2b53d371e6fde6
Bhavya Jain <jainbhavya375@gmail.com>
2020-07-17 18:52:39 +0530

    made the django project

---
+++ Django/tut_project/hello/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("bhavya",views.bhavya,name="bhavya"),
+    path("<str:name>",views.greet,name="greet")
+]
