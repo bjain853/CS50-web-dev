099ffc2f69bb0181ba7f91fdb101fdedfacd34c6
Bhavya Jain <jainbhavya375@gmail.com>
2020-07-18 09:22:08 +0530

    added gitignore

--- Django/tut_project/todoApp/views.py
+++ Django/tut_project/todoApp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 tasks=["foo","bar","baz"]
 # Create your views here.
+'''
+Return a HttpResponse whose 
+content is filled with the result of 
+calling django.template.loader.render_to_string() 
+with the passed arguments.
+'''
 def index(request):
     return render(request,"todoApp/index.html",{
         "tasks":tasks
